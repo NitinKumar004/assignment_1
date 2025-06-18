10368699edd105604a3c34d12a87b4a57dd03ff7
nitin <nitin.kumar@zop.dev>
2025-06-18 23:22:38 +0530

    add testing module in shape

---
+++ Interface_Implementation/Basic_Imp/main2.go
@@ -0,0 +1,31 @@
+package main
+
+//
+//import (
+//	"fmt"
+//)
+//
+//type shape interface {
+//	area() float64
+//}
+//func Rectange struct{
+//
+//}
+//
+//func cost(s shape) float64{
+//	switch sh := s.(type) {
+//	case "Rectangle":
+//		return 0.04*sh
+//	case Circle:
+//		return 0.42
+//
+//	}
+//}
+//
+//func main(){
+//	var s shape
+//	m:=s.area()
+//	fmt.Println(m)
+//
+//
+//}
---
+++ Interface_Implementation/Basic_Imp/testing_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math"
+	"testing"
+)
+
+func withinTolerance(a, b, e float64) bool {
+	if a == b {
+		return true
+	}
+	d := math.Abs(a - b)
+	if b == 0 {
+		return d < e
+	}
+	return (d / math.Abs(b)) < e
+}
+
+func TestRectangle(t *testing.T) {
+	r := &Rectangle{width: 10, height: 5}
+	var exp float64 = 2
+	y := costt(r)
+	if !withinTolerance(exp, y, 1e-12) {
+		t.Errorf("Expected %.18f, got %.18f", exp, y)
+	}
+}
+func TestCircle(t *testing.T) {
+	r := &Circle{5}
+	var exp float64 = 392.6990816
+	y := costt(r)
+
+	if 1e-2 < (exp - y) {
+		t.Errorf("Expected %.18f, got %.18f", exp, y)
+	}
+	//if !withinTolerance(exp, y, 1e-12) {
+	//	t.Errorf("Expected %.18f, got %.18f", exp, y)
+	//}
+}
